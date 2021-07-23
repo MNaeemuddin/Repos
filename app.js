e805a8c76f6edc1bee923ec931af1cc3a5cd2e18
MNaeemuddin <85684290+MNaeemuddin@users.noreply.github.com>
2021-07-23 18:22:52 -0400

    Changed

---
+++ app.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Naeem's JavaScript</title>
+        <style>
+
+        </style>
+    </head>
+    
+        <body>
+<script>
+    alert ("hello,world!");
+    </script>
+
+    </body>
+
+
+</html>
